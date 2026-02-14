5f1716b736363bb162c4c8fa13e3e390196c29fb
Mr.Opu <mahfujurrr492@gmail.com>
2026-02-14 15:27:37 +0600

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDR6FmulLTxxVUvZEcSDGeutHst-H__duY",
-  authDomain: "earnify-money.firebaseapp.com",
-  projectId: "earnify-money",
-  storageBucket: "earnify-money.firebasestorage.app",
-  messagingSenderId: "656847837190",
-  appId: "1:656847837190:web:519a109fef9ef402e14b87",
-  measurementId: "G-5BBJL7679Z"
+  apiKey: "AIzaSyBRtu3m76E_iMzt4qxT_ZA6ePwDZr6PVPM",
+  authDomain: "earnify-0.firebaseapp.com",
+  projectId: "earnify-0",
+  storageBucket: "earnify-0.firebasestorage.app",
+  messagingSenderId: "939562179015",
+  appId: "1:939562179015:web:50af680cc0b2a52041e1e2",
+  measurementId: "G-2K89P4XX6F"
 };
 
 // Initialize Firebase
